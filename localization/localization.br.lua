5837629bcf0e1faf3730aaa71605b083a0c50da3
Jaliborc <contact@jaliborc.com>
2011-11-02 14:32:15 +0000

    Added portuguese translation (done by myself, of course)

---
+++ localization/localization.br.lua
@@ -0,0 +1,56 @@
+--[[
+	Bagnon Localization Information: Brasilian Portuguese
+--]]
+
+local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon', 'ptBR')
+
+--keybinding text
+L.ToggleBags = 'Abrir sacos'
+L.ToggleBank = 'Abrir banco'
+
+
+--system messages
+L.NewUser = 'Novo utilizador detetado, configuração padrão carregada'
+L.Updated = 'Atualizado para versão %s'
+L.UpdatedIncompatible = 'Impossível atualizar para nova versão, configuração padrão carregada'
+
+
+--slash commands
+L.Commands = 'Commandos:'
+L.CmdShowInventory = 'Abre os seus sacos'
+L.CmdShowBank = 'Abre o seu banco'
+L.CmdShowVersion = 'Mostra a versão atual'
+
+
+--frame text
+L.TitleBags = 'Sacos do %s'
+L.TitleBank = 'Banco do %s'
+
+
+--tooltips
+L.TipBags = 'Sacos'
+L.TipBank = 'Banco'
+L.TipBankToggle = '<Clique Direito> para abrir o banco.'
+L.TipChangePlayer = 'Carregue para ver os itens das suas personagens.'
+L.TipGoldOnRealm = '%s no Total'
+L.TipHideBag = 'Clique para esconder o saco.'
+L.TipHideBags = '<Clique Esquerdo> para esconder a lista de sacos.'
+L.TipHideSearch = 'Clique para esconder o campo de pesquisa.'
+L.TipInventoryToggle = '<Clique Direito> para abrir os sacos.'
+L.TipPurchaseBag = 'Clique para comprar uma ranhura no banco.'
+L.TipShowBag = 'Clique para mostrar o bag.'
+L.TipShowBags = '<Clique Esquerdo> para mostrar a lista de sacos.'
+L.TipShowMenu = '<Clique Esquerdo> para configurar esta janela.'
+L.TipShowSearch = 'Clique para pesquisar.'
+L.TipShowFrameConfig = 'Clique para configurar esta janela.'
+L.TipDoubleClickSearch = '<Alt-Arraste> para mover.\n<Clique Direito> para configurar.\n<Duplo Clique> para pesquisar.'
+
+--itemcount tooltips
+L.TipCount1 = ', Equipado: %d'
+L.TipCount2 = ', Sacos: %d'
+L.TipCount3 = ', Banco: %d'
+
+--databroker tooltips
+L.TipShowBank = '<Shift-Clique Esquerdo> para abrir o banco.'
+L.TipShowInventory = '<Clique Esquerdo> para abrir os sacos.'
+L.TipShowOptions = '<Clique Direito> para abrir o menu de configuração.'
