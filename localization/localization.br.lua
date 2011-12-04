4bb980a1df5daadc669ad5f67eaec9368b346895
Jaliborc <contact@jaliborc.com>
2011-12-04 18:21:36 +0000

    Closes #65

--- localization/localization.br.lua
+++ localization/localization.br.lua
@@ -6,7 +6,7 @@ local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon', 'ptBR')
 if not L then return end
 
 --keybinding text
-L.ToggleBags = 'Abrir sacos'
+L.ToggleBags = 'Abrir mochila'
 L.ToggleBank = 'Abrir banco'
 
 
@@ -18,29 +18,29 @@ L.UpdatedIncompatible = 'Impossível atualizar para nova versão, configuração
 
 --slash commands
 L.Commands = 'Commandos:'
-L.CmdShowInventory = 'Abre os seus sacos'
+L.CmdShowInventory = 'Abre a sua mochila'
 L.CmdShowBank = 'Abre o seu banco'
 L.CmdShowVersion = 'Mostra a versão atual'
 
 
 --frame text
-L.TitleBags = 'Sacos de %s'
+L.TitleBags = 'Mochila de %s'
 L.TitleBank = 'Banco de %s'
 
 
 --tooltips
-L.TipBags = 'Sacos'
+L.TipBags = 'Mochila'
 L.TipBank = 'Banco'
 L.TipBankToggle = '<Clique Direito> para abrir o banco.'
 L.TipChangePlayer = 'Carregue para ver os itens das suas personagens.'
 L.TipGoldOnRealm = '%s no Total'
 L.TipHideBag = 'Clique para esconder o saco.'
-L.TipHideBags = '<Clique Esquerdo> para esconder a lista de sacos.'
+L.TipHideBags = '<Clique Esquerdo> para esconder os sacos.'
 L.TipHideSearch = 'Clique para esconder o campo de pesquisa.'
-L.TipInventoryToggle = '<Clique Direito> para abrir os sacos.'
+L.TipInventoryToggle = '<Clique Direito> para abrir a mochila.'
 L.TipPurchaseBag = 'Clique para comprar uma ranhura no banco.'
 L.TipShowBag = 'Clique para mostrar o bag.'
-L.TipShowBags = '<Clique Esquerdo> para mostrar a lista de sacos.'
+L.TipShowBags = '<Clique Esquerdo> para mostrar os sacos.'
 L.TipShowMenu = '<Clique Esquerdo> para configurar esta janela.'
 L.TipShowSearch = 'Clique para pesquisar.'
 L.TipShowFrameConfig = 'Clique para configurar esta janela.'
@@ -48,10 +48,10 @@ L.TipDoubleClickSearch = '<Alt-Arraste> para mover.\n<Clique Direito> para confi
 
 --itemcount tooltips
 L.TipCount1 = ', Equipado: %d'
-L.TipCount2 = ', Sacos: %d'
+L.TipCount2 = ', Mochila: %d'
 L.TipCount3 = ', Banco: %d'
 
 --databroker tooltips
 L.TipShowBank = '<Shift-Clique Esquerdo> para abrir o banco.'
-L.TipShowInventory = '<Clique Esquerdo> para abrir os sacos.'
+L.TipShowInventory = '<Clique Esquerdo> para abrir a mochila.'
 L.TipShowOptions = '<Clique Direito> para abrir o menu de configuração.'
