4a48b6abcbd3e04868f0e9962895163379c1927d
Jaliborc <contact@jaliborc.com>
2012-01-28 12:15:25 +0000

    Updates localization.tw

--- localization/localization.tw.lua
+++ localization/localization.tw.lua
@@ -1,14 +1,15 @@
 --[[
-THIS FILE IS ENCODED IN UTF-8
+	THIS FILE IS ENCODED IN UTF-8
 
-Bagnon Localization Information: Chinese Traditional
-2007/11/17 by matini< yiting.jheng <at> gmail <dot> com
-2008/12/01 by yleaf@cwdg(yaroot@gmail.com)
-2009/04/23 by youngway@水晶之刺
-2009/06/19 by 狂飆@cwdg(networm@qq.com)
-2011/07/06 by Seraveegd@鬼霧峰
+	Bagnon Localization Information: Chinese Traditional
+	        2007/11/17 by matini< yiting.jheng <at> gmail <dot> com
+	        2008/12/01 by yleaf@cwdg(yaroot@gmail.com)
+	        2009/04/23 by youngway@水晶之刺
+			    2009/06/19 by 狂飆@cwdg(networm@qq.com)
+          2011/07/06 by Seraveegd@鬼霧峰
+          2012/01/24 by Seraveegd@鬼霧峰
 
-Last Update: 2011/07/06 by Seraveegd@鬼霧峰
+	Last Update: 2012/01/24 by Seraveegd@鬼霧峰
 
 --]]
 
@@ -42,22 +43,27 @@ L.TitleBank = '%s的銀行'
 L.TipBags = '背包'
 L.TipBank = '銀行'
 L.TipBankToggle = '<右鍵點擊> 切換銀行。'
-L.TipChangePlayer = '點擊檢視其他角色的物品'
+L.TipChangePlayer = '點擊檢視其他角色的物品。'
 L.TipGoldOnRealm = '%s上的總資產'
-L.TipHideBag = '點擊隱藏背包'
-L.TipHideBags = '<左鍵點擊>隱藏背包顯示'
-L.TipHideSearch = '點擊隱藏搜尋介面'
-L.TipInventoryToggle = '<右鍵點擊>切換背包'
-L.TipPurchaseBag = '點擊購買銀行槽'
-L.TipShowBag = '點擊顯示背包'
-L.TipShowBags = '<左鍵點擊>顯示背包顯示'
-L.TipShowMenu = '<右鍵點擊>設定視窗'
-L.TipShowSearch = '點擊搜尋'
-L.TipShowFrameConfig = '打開設定視窗'
-L.TipDoubleClickSearch = '<Alt-拖動>移動\n<右鍵點擊>設定\n<兩次點擊>搜尋'
+L.TipHideBag = '點擊隱藏背包。'
+L.TipHideBags = '<左鍵點擊>隱藏背包顯示。'
+L.TipHideSearch = '點擊隱藏搜尋介面。'
+L.TipInventoryToggle = '<右鍵點擊>切換背包。'
+L.TipPurchaseBag = '點擊購買銀行槽。'
+L.TipShowBag = '點擊顯示背包。'
+L.TipShowBags = '<左鍵點擊>顯示背包顯示。'
+L.TipShowMenu = '<右鍵點擊>設定視窗。'
+L.TipShowSearch = '點擊搜尋。'
+L.TipShowFrameConfig = '打開設定視窗。'
+L.TipDoubleClickSearch = '<Alt-拖動>移動。\n<右鍵點擊>設定。\n<兩次點擊>搜尋。'
 L.Total = '總共'
 
+--itemcount tooltips
+L.TipCount1 = ', 已裝備: %d'
+L.TipCount2 = ', 背包: %d'
+L.TipCount3 = ', 銀行: %d'
+
 --databroker plugin tooltips
-L.TipShowBank = '<Shift-左鍵點擊>切換銀行'
-L.TipShowInventory = '<左鍵點擊>切換背包'
-L.TipShowOptions = '<右鍵點擊>打開設定選單'
+L.TipShowBank = '<Shift-左鍵點擊>切換銀行。'
+L.TipShowInventory = '<左鍵點擊>切換背包。'
+L.TipShowOptions = '<右鍵點擊>打開設定選單。'
