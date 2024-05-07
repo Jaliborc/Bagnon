435c042b18db2ddfd0806e650e63985cbdcf1200
João Cardoso <jaliddons@gmail.com>
2024-05-07 23:56:43 +0100

    OnePixel skin

--- src/main.lua
+++ src/main.lua
@@ -4,18 +4,17 @@
 
 local ADDON, Addon = ...
 local apply9S = function(name) return function(f) NineSliceUtil.ApplyLayout(f, NineSliceLayouts[name]) end end
-local centerBG = function(f, ...) f.BG:SetColorTexture(...) end
-local center9S = function(f, ...) f:SetCenterColor(...) end
-local border9S = function(f, ...) f:SetBorderColor(...) end
+local center9S = NineSlicePanelMixin.SetCenterColor
+local border9S = NineSlicePanelMixin.SetBorderColor
 
 Addon.Slash = 'bgn'
 Addon.Skins.Default = 'Bagnon'
-Addon.Skins:Register { id = 'Bagnon', template = 'NineSliceCodeTemplate', load = apply9S('TooltipDefaultLayout'), borderColor = border9S, centerColor = center9S }
+Addon.Skins:Register { id = 'Bagnon', template = 'NineSliceCodeTemplate', load = apply9S('TooltipDefaultLayout'), centerColor = center9S, borderColor = border9S }
 Addon.Skins:Register { id = 'Barber', template = 'NineSliceCodeTemplate', load = apply9S('CharacterCreateDropdown'), borderColor = border9S, x=-1,y=-20,y1=1 }
 Addon.Skins:Register { id = 'Bubble', template = 'NineSliceCodeTemplate', load = apply9S('ChatBubble'), borderColor = border9S }
-Addon.Skins:Register { id = 'Dialog', template = 'BagnonDialogSkinTemplate', borderColor = border9S, centerColor = centerBG, x=-7,y=-7, x1=7,y1=7 }
-Addon.Skins:Register { id = 'Inset', template = 'BagnonInsetSkinTemplate', centerColor = centerBG }
-Addon.Skins:Register { id = 'One Pixel', template = 'OnePixel', centerColor = centerBG}
+Addon.Skins:Register { id = 'Dialog', template = 'BagnonDialogSkinTemplate', centerColor = center9S, borderColor = border9S, x=-7,y=-7, x1=7,y1=7 }
+Addon.Skins:Register { id = 'Inset', template = 'BagnonInsetSkinTemplate', centerColor = center9S }
+Addon.Skins:Register { id = 'OnePixel', template = 'BagnonOnePixelTemplate', centerColor = center9S, borderColor = border9S }
 
 Addon.Skins:Register {
 	id = 'Panel - Flat', template = 'DefaultPanelFlatTemplate', font = GameFontNormalCenter, fontH = GameFontHighlightCenter,
@@ -34,7 +33,7 @@ Addon.Skins:Register {
 
 Addon.Skins:Register {
 	id = 'Thin', template = 'HelpPlateBox', font = GameFontHighlightLeft,
-	centerColor = centerBG,
+	centerColor = function(f, ...) f.BG:SetColorTexture(...) end,
 	borderColor = function(f, ...)
 		for i = 1, select('#', f:GetRegions()) do
 			local texture = select(i, f:GetRegions())
